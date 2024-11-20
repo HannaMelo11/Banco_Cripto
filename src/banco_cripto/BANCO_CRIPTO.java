e3040c53432221adc5cf3c56f6a3ba111c6b5647
Hanna <Hanna@DESKTOP-EBNFKAG>
2024-11-20 14:42:15 -0300

    Primeiro commit - Projeto BANCO DE CRIPTOMOEDAS

---
+++ src/banco_cripto/BANCO_CRIPTO.java
@@ -0,0 +1,10 @@
+package banco_cripto;
+import banco_cripto.view.Cadastro;
+
+public class BANCO_CRIPTO {
+    public static void main(String[] args) {
+        Cadastro cadastroFrame = new Cadastro();
+        cadastroFrame.setVisible(true);      
+        
+    }
+}
